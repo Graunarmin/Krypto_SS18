ca1c8a5c162a535d7fd17f6b60292e10a2baec88
Johanna Sacher <johanna.sacher@uni-weimar.de>
2018-05-26 20:21:46 +0200

    🧞‍♀️

--- Blatt4/ddt.py
+++ Blatt4/ddt.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 '''Need the string statements to explain, leave me alone, pylint -.-'''
 
-''' Terminal: python3 ddt.py "sbox.txt" 
-    bzw       python3 ddt.py "sbox4.txt" für Nr. 4 a) '''
-
 import sys
 #from pprint import pprint
+
+''' Terminal: python3 ddt.py "sbox.txt"
+    bzw       python3 ddt.py "sbox4.txt" für Nr. 4 a) '''
 
 def read_file(filename):
     '''
@@ -27,26 +27,20 @@ def read_file(filename):
 
 
 def create_ddt(sbox):
-    '''create ddt by testing all possible inputs'''
+    '''
+    create ddt by testing all possible inputs
+    '''
 
     size = len(sbox)
-    delta_in = []
-    delta_out = []
-    x_werte = []
-
-
-    for y_i in range(0, size):
-        delta_in.append(y_i)
-        delta_out.append(y_i)
-        x_werte.append(y_i)
+    values = [y for y in range(size)]
 
     ddt_s = [[0 for x in range(size)] for y in range(size)]
 
-    for a_i in delta_in:
-        for b_i in delta_out:
-            for x_i in x_werte:
-                if (sbox[x_i] ^ sbox[x_i ^ a_i]) == b_i:
-                    ddt_s[b_i][a_i] += 1
+    for d_in in values:
+        for d_out in values:
+            for x_i in values:
+                if (sbox[x_i] ^ sbox[x_i ^ d_in]) == d_out:
+                    ddt_s[d_out][d_in] += 1
 
     print_ddt(ddt_s, size)
 
@@ -58,23 +52,17 @@ def print_ddt(ddt, size):
 
     output = [[0 for x in range(size+1)] for y in range(size+1)] # w x h
 
-    '''add delta_in in first row and delta_out in first column'''
-    for w_i in range(0, size+1):
-        for h_i in range(0, size+1):
+    '''add delta_in in first row and delta_out in first column and fill with leading zeros'''
+    for w_i in range(0, size + 1):
+        for h_i in range(0, size + 1):
             if w_i == 0 and h_i == 0:
-                output[w_i][h_i] = 0
+                output[w_i][h_i] = (str(0)).zfill(2)
             elif w_i == 0 and h_i != 0:
-                output[w_i][h_i] = h_i - 1
+                output[w_i][h_i] = (str(h_i - 1)).zfill(2)
             elif h_i == 0 and w_i != 0:
-                output[w_i][h_i] = w_i - 1
+                output[w_i][h_i] = (str(w_i - 1)).zfill(2)
             else:
-                output[w_i][h_i] = ddt[w_i - 1][h_i - 1]
-
-    '''fill with 2 digit numbers'''
-    for w_i in range(0, size+1):
-        for h_i in range(0, size+1):
-            output[w_i][h_i] = (str(output[w_i][h_i])).zfill(2)
-
+                output[w_i][h_i] = (str(ddt[w_i - 1][h_i - 1])).zfill(2)
 
     print("DDT:")
     for w_i in range(0, size+1):
@@ -83,13 +71,14 @@ def print_ddt(ddt, size):
             if h_i == size:
                 print("\n")
 
-
 def error():
-    """Error-Function in case of false userinput"""
+    '''
+    Error-Function in case of false userinput
+    '''
 
     if len(sys.argv) != 2:
         print('ERROR: \n',
-              'Please enter a valid path to the sbox.txt (without -i please)')
+              'Please enter a valid path to the "sbox.txt" (without -i please)')
         sys.exit(1)
 
 
