4d155b3c245d72e67ee4edb2239085979cf3ca84
Johanna Sacher <johanna.sacher@uni-weimar.de>
2018-06-24 15:11:26 +0200

    3 working :)

--- Blatt6/nummer_3.py
+++ Blatt6/nummer_3.py
@@ -4,6 +4,7 @@ from typing import List
 def get_key():
 
     # gegeben:
+    print("gegeben:")
     I_V = 0xdabe785fb5a688a61cc77c3035d754fa
     C_1 = 0x5973ecca8de35880102bd68ba0ede12b
     C_2 = 0x42f2dc27a8a676d69e1825d323016d38
@@ -29,6 +30,7 @@ def get_key():
     # Ek(IV) XOR m_1 = c_1
     # M_1 XOR C_1 XOR m_1 = c_1
 
+    print("a)")
     c_1 = M_1 ^ C_1 ^ m_1 
     print("c_1 = {:04x}".format(c_1))
 
@@ -36,6 +38,15 @@ def get_key():
     # IV = 0xdabe785fb5a688a61cc77c3035d754fa
     # c1 = 0x5973ecca8de35f99172bd28ba0ede12b
     # c2 = 0x42f2dc27a8a676d69e1825d323016d38
+
+    # überprüfen:
+    mes_1 = c_1 ^ M_1 ^ C_1
+    print("mes_1 = {:04x}".format(mes_1))
+    #Ergebnis von mes_1 aus terminal kopieren und einsetzen: (geht bestimmt auch eleganter, weiß gerad nicht wie^^)
+    mes_1 = binascii.unhexlify('5a61686c652045766520353030455552')
+    print("Neue Nachricht Teil 1:", mes_1)
+
+
 
 
 # b) CBC Mode:
@@ -45,9 +56,24 @@ def get_key():
     # hier muss iv geändert werden:
     # c1 = Ek(iv XOR m1)
     # c2 = Ek(c1 XOR M2)
+    # gut wäre, wenn Ek(IV XOR M1) == Ek(iv XOR m1), damit c1 == C1 - anders kommt man da nicht dran. 
+    # also: iv XOR m1 == IV XOR M1
+    # Es gilt m2 == M2 und wenn c1 == C1, dann gilt wegen c2 = Ek(c1 XOR m1) = Ek(C1 XOR M1) = C2
 
+    print("b)")
+    res = I_V ^ M_1
+    iv = m_1 ^ res
 
+    print("iv = {:04x}".format(iv))
 
+    #überprüfen: (eigl reduntant, ist ja nur umstellen und beweist nix ... aber funktioniert :D)
+    message_1 = I_V ^ iv ^ M_1
+    print("message_1 = {:04x}".format(message_1))
+    message_1 = binascii.unhexlify('5a61686c652045766520353030455552')
+    print("Neue Nachricht Teil 1:", message_1)
 
+    # iv = 0xdabe785fb5a68fbf1bc7783035d754fa
+    # c1 = C1 = 0x5973ecca8de35880102bd68ba0ede12b
+    # c2 = C2 = 0x42f2dc27a8a676d69e1825d323016d38
 
 get_key()
